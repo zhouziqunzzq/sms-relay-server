dbc7b4b02e0200ecf738d9f7d4a10e8132093c83
Harry Zhou <zhouziqun@cool2645.com>
2025-07-14 00:42:57 -0700

    Add models

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhouziqunzzq/sms-relay-server
+
+go 1.24.5
---
+++ models/sms.go
@@ -0,0 +1,13 @@
+package models
+
+type SMS struct {
+	ID string `json:"id"` // UUID of the SMS message
+
+	From string `json:"from"` // Phone number ID of the sender, in E.164 format
+	Body string `json:"body"` // Content of the SMS message, AES-256 encrypted
+
+	PhoneNumberID string `json:"phone_number_id"` // ID of the phone number associated with this SMS
+
+	ReceivedAt string `json:"received_at,omitempty"` // Timestamp of when the SMS was received by the device
+	CreatedAt  string `json:"created_at,omitempty"`  // Timestamp of when the SMS entry was created in the database
+}
